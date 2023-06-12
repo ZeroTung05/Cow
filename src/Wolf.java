374c8b1b1f0609849af4b0bd84825c760a74c4b6
ZeroTung05 <s.ch2605@mail.ru>
2023-06-12 15:26:03 +0300

    wolf add

---
+++ src/Wolf.java
@@ -0,0 +1,24 @@
+public class Wolf extends Animal {
+    private boolean fangs;
+
+    public Wolf(String name, int eyes, int legs, boolean tail, int arms, boolean skin, boolean fangs) {
+        super(name, eyes, legs, tail, arms, skin);
+        this.fangs = fangs;
+    }
+
+    public boolean bites(){return true;
+    }
+
+    public void isFangs() {
+        if (fangs) {
+            System.out.println("У волка " + getName() + " есть клыки!");
+        }
+        else {
+            System.out.println("У волка " + getName() + " нет клыков :(");
+        }
+    }
+
+    public void setFangs(boolean fangs) {
+        this.fangs = fangs;
+    }
+}
